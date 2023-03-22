fe3fd23fc2cef4acb7d821404ef17605b49cf0ff
Maurizio <mauriziodm91@gmail.com>
2023-03-22 14:41:29 -0300

    fixed completed field validator

--- services/utils.js
+++ services/utils.js
@@ -3,7 +3,10 @@ const { body } = require('express-validator')
 const validators = [
   body('name').notEmpty().withMessage('Name is required'),
   body('description').optional(),
-  body('completed').isBoolean().withMessage('Completed must be a boolean'),
+  body('completed')
+    .optional()
+    .isBoolean()
+    .withMessage('Completed must be a boolean'),
 ]
 
 module.exports = validators
