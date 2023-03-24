c785ad83ba4b45a2b29d95d132b3a3e8f1e3f150
Maurizio <mauriziodm91@gmail.com>
2023-03-24 00:39:39 -0300

    created username validator middleware for /v1/user endpoint

--- routes/user/user.router.js
+++ routes/user/user.router.js
@@ -1,18 +1,28 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const userRouter = express.Router()
 
-userRouter.post('/login', (req, res) => {
-  //IMPORTANT: NO DATA SHOULD BE HARDCODED THIS IS MADE JUST FOR DEMONSTRATION PURPOSES
-  const username = req.body.username
-  const user = { name: username }
+//THIS WHOLE ENDPOINT IS FOR DEMONSTRATION PURPOSES
+userRouter.post(
+  '/login',
+  body('username').notEmpty().withMessage('Please enter a username'),
+  (req, res) => {
+    //IMPORTANT: NO DATA SHOULD BE HARDCODED THIS IS MADE JUST FOR DEMONSTRATION PURPOSES
+    const error = validationResult(req)
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() })
+    }
+    const username = req.body.username
+    const user = { name: username }
 
-  const accessToken = jwt.sign(user, process.env.JWT_SECRET, {
-    expiresIn: '1800s',
-  })
+    const accessToken = jwt.sign(user, process.env.JWT_SECRET, {
+      expiresIn: '1800s',
+    })
 
-  res.json({ accessToken: accessToken })
-})
+    res.json({ accessToken: accessToken })
+  }
+)
 
 module.exports = userRouter
